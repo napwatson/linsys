d0de584ee61c065131b3fce9e05694ba5c207d06
nap watson <napwatson@gmail.com>
2019-11-19 14:49:25 +0900

    Update local.c

--- local.c
+++ local.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
-main ()
-{
-printf ("This is Testing C program. ") ;
-return 0;
-}
+URL : 
+registory.terraform.io
+
+Recomm: 
+- Always include the tf version tf files
+
+module versioning is also available
+
+
+
+Terraform CLI: 
+
+>terraform plan
+>terrform init
+>terraform apply
+>terraform show
+>terraform help
+>terraform version
+>terraform -h plan
+>terraform destroy
+
+
+
+
